7fbcb4287e7f0524001e20d33e36316ac8d6132f
admins <admins@DESKTOP-B95IMFI>
2021-06-10 16:20:03 +0900

    202106101619

---
+++ shopping/src/controller/employee/EmployeeController.java
@@ -0,0 +1,34 @@
+package controller.employee;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EmployeeController extends HttpServlet
+			implements Servlet{
+	
+	public void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String requestURI=request.getRequestURI();
+		String contextPath=request.getContextPath();
+		String command=requestURI.substring(contextPath.length());
+		
+		if(command.equals("/empList.em")) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("employee/employeeList.jsp");
+			dispatcher.forward(request, response); // 여기서 exception걸어줌
+		}
+	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
+	}
+}

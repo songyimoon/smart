04e29a643797262b964c68d2d0430de8767f5ed2
admins <admins@DESKTOP-B95IMFI>
2021-06-11 16:30:23 +0900

    202106111630

--- shopping/WebContent/employee/employeeList.jsp
+++ shopping/WebContent/employee/employeeList.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %> 
+ 
+ 
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,6 +11,20 @@
 </head>
 <body>
 	직원 리스트 페이지입니다.<br/>
-	<a href="#">직원등록</a>
+	<table border = "1">
+		<tr><td>사원번호</td>
+			<td>이름</td>
+			<td>직무</td>
+			<td>입사일</td>
+			<td>사무실번호</td></tr>
+	<c:forEach items="${empList }" var="dto">		
+		<tr><td>${dto.employeeId}</td>
+			<td>${dto.empName}</td>
+			<td>${dto.jobId}</td>
+			<td>${dto.hireDate}</td>
+			<td>${dto.officeNumber}</td></tr>
+	</c:forEach>
+	</table>
+	<a href="empRegest.em">직원등록</a>
 </body>
 </html>
--- shopping/src/controller/employee/EmployeeController.java
+++ shopping/src/controller/employee/EmployeeController.java
@@ -19,10 +19,22 @@ public class EmployeeController extends HttpServlet
 		String command=requestURI.substring(contextPath.length());
 		
 		if(command.equals("/empList.em")) {
+			EmployeeListPage action = new EmployeeListPage();
+			action.empList(request);
 			RequestDispatcher dispatcher = request.getRequestDispatcher("employee/employeeList.jsp");
 			dispatcher.forward(request, response); // 여기서 exception걸어줌
+		}else if (command.equals("/empRegest.em")) {
+			EmployeeNumPage action = new EmployeeNumPage();
+			action.getNum(request);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("employee/employeeForm.jsp");
+			dispatcher.forward(request, response);
+		}else if (command.equals("/empJoin.em")) {
+			EmployeeJoinPage action = new EmployeeJoinPage();
+			action.empInsert(request); // pageController에 전달하기 위한 메서드를 만들어서, 그를 통해서 request를 전달한다.
+			response.sendRedirect("empList.em");
 		}
 	}
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doProcess(req, resp);
